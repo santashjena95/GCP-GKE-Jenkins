412003dc7efb17a7b505d9a6335e4cd987b2dbf9
santashjena95 <jenasantash95@gmail.com>
2024-09-08 11:46:17 +0530

    resolved one string issue

--- module/gcr.tf
+++ module/gcr.tf
@@ -1,7 +1,7 @@
 resource "google_artifact_registry_repository" "googleartifactregistry" {
     for_each      = var.repository_name
     location      = var.region
-    repository_id = "${substr(var.environment, 0, 1)}${var.channel_name}${var.portfolio_name}-${each.value}}"
+    repository_id = "${substr(var.environment, 0, 1)}${var.channel_name}${var.portfolio_name}-${each.value}"
     description   = "Artifact Registry"
     format        = "Docker"
 }
